28d539dfb5bfa8ae076427d9579a273714684de1
Killian <paulic.killian@gmail.com>
2023-04-16 16:44:33 +0200

    navigation through navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import { Router } from './pages/router';
+import { NavigationBar } from './components/navigationBar/navigationBar';
 
 function App() {
   return (
     <div>
+      <NavigationBar />
       <Router />
     </div>
   );
---
+++ src/components/navigationBar/navigationBar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./navigationBar.css";
+import { useLocation, useNavigate } from "react-router-dom";
+
+export const NavigationBar = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleClick = (destination) => {
+    console.log(destination);
+    if (location.pathname !== destination) {
+      navigate(destination);
+    }
+  };
+
+  return (
+    <nav className="navigation-bar">
+      <div
+        className="navigation-website-title"
+        onClick={() => handleClick("/")}
+      >
+        My name
+      </div>
+      <div className="navigation-right">
+        <div className="navigation-element" onClick={() => handleClick("/")}>
+          Articles
+        </div>
+        <div
+          className="navigation-element"
+          onClick={() => handleClick("/portfolio")}
+        >
+          Portfolio
+        </div>
+        <div
+          className="navigation-element"
+          onClick={() => handleClick("/aboutMe")}
+        >
+          About Me
+        </div>
+        <button>Create article</button>
+      </div>
+    </nav>
+  );
+};
