1e07187931877072f8ae0935ef819c37fe1194a5
Killian <paulic.killian@gmail.com>
2023-04-19 10:47:26 +0200

    form structure

--- src/pages/createArticles/createArticles.jsx
+++ src/pages/createArticles/createArticles.jsx
@@ -6,7 +6,21 @@ export const CreateArticles = () => {
   return (
     <div>
       <h1 className="page-title">Create article</h1>
-      
+      <div>
+        <div>
+          <div>
+            <h2>title</h2>
+            <input type="text" />
+          </div>
+          <div>
+            <h2>picture url</h2>
+            <input type="url" />
+          </div>
+          <h2>Article</h2>
+          <input type="text" />
+        </div>
+        <button>Submit</button>
+      </div>
     </div>
   );
-}
+};
