cb75c2a8161b0f4d901646387d0540767db66ef8
Killian <paulic.killian@gmail.com>
2023-04-19 17:48:54 +0200

    create article functionnal

--- src/pages/createArticles/createArticles.jsx
+++ src/pages/createArticles/createArticles.jsx
@@ -5,6 +5,18 @@ import { articlesList } from "../../data/articles";
 export const CreateArticles = () => {
 
   const submitArticle = () => {
+    const title = document.querySelector("input[name='title']").value;
+    const imageUrl = document.querySelector("input[name='picture']").value;
+    const content = document.querySelector("textarea[name='content']").value;
+
+    const newArticle = {
+      title,
+      imageUrl,
+      content,
+      id: articlesList.length + 1,
+    };
+
+    articlesList.push(newArticle);
   }
 
   return (
@@ -15,22 +27,22 @@ export const CreateArticles = () => {
           <div className="create-article-top-input">
             <div>
               <h2>Title</h2>
-              <input type="text" className="create-article-input" />
+              <input type="text" className="create-article-input" name="title" />
             </div>
             <div>
               <h2>Picture URL</h2>
-              <input type="url" className="create-article-input" />
+              <input type="url" className="create-article-input" name="picture" />
             </div>
           </div>
           <h2>Article</h2>
           <textarea
-            name="article content"
+            name="content"
             rows="8"
             cols="100"
             className="create-article-text"
           />
         </div>
-        <button>Submit</button>
+        <button onClick={() => submitArticle()}>Submit</button>
       </div>
     </div>
   );
