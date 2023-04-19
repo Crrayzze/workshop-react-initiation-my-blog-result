c75f2e953e0f7c9a6bc14bac995689de97ee208c
Killian <paulic.killian@gmail.com>
2023-04-19 17:04:56 +0200

    create article page

--- src/pages/createArticles/createArticles.jsx
+++ src/pages/createArticles/createArticles.jsx
@@ -3,21 +3,32 @@ import "./createArticles.css";
 import { articlesList } from "../../data/articles";
 
 export const CreateArticles = () => {
+
+  const submitArticle = () => {
+  }
+
   return (
     <div>
       <h1 className="page-title">Create article</h1>
-      <div>
+      <div className="create-article-center">
         <div>
-          <div>
-            <h2>title</h2>
-            <input type="text" />
-          </div>
-          <div>
-            <h2>picture url</h2>
-            <input type="url" />
+          <div className="create-article-top-input">
+            <div>
+              <h2>Title</h2>
+              <input type="text" className="create-article-input" />
+            </div>
+            <div>
+              <h2>Picture URL</h2>
+              <input type="url" className="create-article-input" />
+            </div>
           </div>
           <h2>Article</h2>
-          <input type="text" />
+          <textarea
+            name="article content"
+            rows="8"
+            cols="100"
+            className="create-article-text"
+          />
         </div>
         <button>Submit</button>
       </div>
